3dd84edd97c123ccbdd7812068036b90c44ccad8
Soham Powar <173037437+Soham-Powar@users.noreply.github.com>
2025-06-30 14:05:10 +0530

    feat: update NavBar component layout and styles

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,16 +1,18 @@
 import NavLink from "./NavLink";
+import SearchBar from "./SearchBar";
 import { ShoppingCart } from "lucide-react";
 
 const NavBar = () => {
 	return (
-		<nav className="bg-white py-5 flex text-2xl font-extrabold p-3 gap-2 justify-around">
-			<p>SHOP.EZ</p>
+		<nav className="bg-white py-5 flex p-3 gap-2 justify-around items-center">
+			<p className="text-2xl font-extrabold">SHOP.EZ</p>
 			<div>
 				<NavLink name="Home" />
 				<NavLink name="Shop" />
 				<NavLink name="New Arrivals" />
 				<NavLink name="Brands" />
 			</div>
+			<SearchBar />
 			<ShoppingCart />
 		</nav>
 	)
