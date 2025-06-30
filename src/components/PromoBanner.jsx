c24a186745f5151bba2fc661ee6f74e5fe51355f
Soham Powar <173037437+Soham-Powar@users.noreply.github.com>
2025-06-30 13:19:04 +0530

    feat: add PromoBanner component and corresponding tests

---
+++ src/components/PromoBanner.jsx
@@ -0,0 +1,9 @@
+const PromoBanner = () => {
+	return (
+		<div className="bg-black text-white">
+			Sign up and get 20% off to your first order. Sign Up Now
+		</div>
+	)
+}
+
+export default PromoBanner;
