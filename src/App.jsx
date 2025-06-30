a0942d0c93ca5001686348e867e3eaf1791f2220
Soham Powar <173037437+Soham-Powar@users.noreply.github.com>
2025-06-30 11:33:59 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+// App.jsx
+
+import { useState } from "react";
+
+const App = () => {
+	const [heading, setHeading] = useState("Magnificent Monkeys");
+
+	const handleClick = () => {
+		setHeading("Radical Rhinos")
+	}
+
+	return (
+		<>
+			<button type="button" onClick={handleClick}>
+				Click Me
+			</button>
+			<h1>{heading}</h1>
+		</>
+	)
+}
+
+export default App;
