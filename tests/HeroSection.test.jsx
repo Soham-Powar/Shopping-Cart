709e12c0fbdf54fc3b1b15b8dea842d30d3cde3e
Soham Powar <173037437+Soham-Powar@users.noreply.github.com>
2025-06-30 21:52:04 +0530

    fix: wrap HeroSection in BrowserRouter

--- tests/HeroSection.test.jsx
+++ tests/HeroSection.test.jsx
@@ -2,10 +2,15 @@ import HeroSection from "../src/components/HeroSection";
 
 import { render, screen } from "@testing-library/react";
 import { describe, it, expect } from "vitest";
+import { BrowserRouter } from "react-router-dom";
 
-describe("Hero Image component", () => {
+describe("Hero Section component", () => {
 	it("renders hero image div", () => {
-		render(<HeroSection />);
+		render(
+			<BrowserRouter>
+				<HeroSection />
+			</BrowserRouter>
+		);
 		const heroDiv = screen.getByTestId("hero-img");
 		expect(heroDiv).toBeInTheDocument();
 	})
