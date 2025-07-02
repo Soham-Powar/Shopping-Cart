6aa7ad3ac14eb59cf29fceab2faaa915313eed3b
Soham Powar <173037437+Soham-Powar@users.noreply.github.com>
2025-07-02 13:03:07 +0530

    feat: Add bottom border to NavBar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,7 +4,7 @@ import { ShoppingCart } from "lucide-react";
 
 const NavBar = () => {
 	return (
-		<nav className="bg-white py-5 flex p-3 gap-2 justify-around items-center">
+		<nav className="bg-white py-5 flex p-3 gap-2 justify-around items-center border-gray-200 border-b-3">
 			<p className="text-3xl font-extrabold font-[integralCF]">SHOP.EZ</p>
 			<div>
 				<NavLink name="Home" />
